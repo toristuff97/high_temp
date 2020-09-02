f1848504558621b57de89f5bc1a9846e92944a38
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-09-02 10:44:24 -0400

    working on cakesandpies comp

---
+++ hightemp/src/Components/CakesAndPies.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+ const CakesAndPies = () => {
+
+    const [cakesAndPies, setCakesAndPies] = useState({
+        name: "",
+        price: ""
+    });
+    
+    
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
