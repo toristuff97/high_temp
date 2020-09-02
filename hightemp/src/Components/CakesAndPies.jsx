f8fa3f82ce81e5280fc1ec661735f7d49cbc3b84
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-09-02 12:12:35 -0400

    working on async

--- hightemp/src/Components/CakesAndPies.jsx
+++ hightemp/src/Components/CakesAndPies.jsx
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/css';
 
  const CakesAndPies = () => {
 
-    const [cakesAndPies, setCakesAndPies] = useState({
-        name: "",
-        price: ""
-    });
+    const [cakesAndPies, setCakesAndPies] = useState({ name: "", price: "" });
     const [cakesAndPies, setCakesAndPies] = useState([]);
     const [cakesAndPiesId, setCakesAndPiesId] = useState([]);
+
+    async function grabCakesAndPies() {
+        try {
+            const res = await
+            axios.get('http://localhost:8080/high_temp/cakes_and_pies');
+            console.log(res.data);
+        } catch(err) {
+            console.error(err, err.message);
+        }
+    }
+
+    useEffect(() => {
+        grabCakesAndPies();
+    }, [])
 
 
 
