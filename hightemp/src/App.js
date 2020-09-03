58d225c0262119dd25e95204d74a1f75ba2fc8a4
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-09-03 13:18:26 -0400

    add router

--- hightemp/src/App.js
+++ hightemp/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './App.css';
-import CakesAndPies from './Components/CakesAndPies';
+import Shop from './Components/Shop';
 import Home from './Components/Home';
+import Custom from './Components/Custom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/shop">
+            <Shop/>
+          </Route>
+
+          <Route path="/custom">
+            <Custom/>
+          </Route>
+
+        </Switch>
+      </Router>
+      
       {/* This is a bootflat test */}
       {/* <a className="btn btn-primary" href="#" >Flat button</a> */}
       {/* <CakesAndPies/> */}
--- hightemp/src/Components/Home.jsx
+++ hightemp/src/Components/Home.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import twitter from '../assets/twitter.png'
-import instagram from '../assets/instagram.png'
-import facebook from '../assets/facebook.png'
+import twitter from '../assets/twitter.png';
+import instagram from '../assets/instagram.png';
+import facebook from '../assets/facebook.png';
+import { BrowserRouter as Router,
+        Route, 
+        Link } from 'react-router-dom';
 
 export default class Home extends Component {
     render() {
@@ -9,7 +12,9 @@ export default class Home extends Component {
             <div className="Home">
                 <div className="navigation" style={{display: 'flex', fontFamily: 'Shrikhand', color: 'white'}}>
                     <h3>Home</h3>
+                    <Link to="/custom" style={{textDecoration: 'none', color: 'white'}}>
                     <h3>Custom</h3>
+                    </Link>
                     <h1>High Temp</h1>
                     <h3>Shop</h3>
                     <h3>Wishlist</h3>
@@ -20,9 +25,9 @@ export default class Home extends Component {
                     <button id="newButton" >What's New</button>
                 </div>
                 <div className="socials">
-                    <a href="http://www.instagram.com"><img className="icon" src={instagram}/></a>
-                    <a href="http://www.twitter.com"><img className="icon" src={twitter}/></a>
-                    <a href="http://www.facebook.com"><img className="icon" src={facebook}/></a>
+                    <a href="http://www.instagram.com"><img className="icon" src={instagram} alt="Link to Instagram"/></a>
+                    <a href="http://www.twitter.com"><img className="icon" src={twitter} alt="Link to Twitter"/></a>
+                    <a href="http://www.facebook.com"><img className="icon" src={facebook} alt="Link to Facebook"/></a>
                 </div>
             </div>
         )
