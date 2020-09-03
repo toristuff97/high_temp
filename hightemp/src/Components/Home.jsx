a49d44dd65eedf3b754d141c21055f35be8f9772
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-09-03 11:29:09 -0400

    added social links

--- hightemp/src/Components/Home.jsx
+++ hightemp/src/Components/Home.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import twitter from '../assets/twitter.png'
+import instagram from '../assets/instagram.png'
+import facebook from '../assets/facebook.png'
 
 export default class Home extends Component {
     render() {
@@ -15,6 +18,11 @@ export default class Home extends Component {
                     <p style={{fontWeight: "bold", fontSize: 40, color: 'white'}}>We're best friends with your sweet tooth.</p>
                     <br></br>
                     <button id="newButton" >What's New</button>
+                </div>
+                <div className="socials">
+                    <a href="http://www.instagram.com"><img className="icon" src={instagram}/></a>
+                    <a href="http://www.twitter.com"><img className="icon" src={twitter}/></a>
+                    <a href="http://www.facebook.com"><img className="icon" src={facebook}/></a>
                 </div>
             </div>
         )
