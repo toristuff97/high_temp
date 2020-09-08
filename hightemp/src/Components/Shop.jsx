fbc42b739bddd3fdfffa193d96de8b7f3d5228b5
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-09-08 09:34:12 -0400

    work on Shop

--- hightemp/src/Components/Shop.jsx
+++ hightemp/src/Components/Shop.jsx
@@ -6,8 +6,26 @@ import { BrowserRouter as Router,
 export default class Shop extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="Shop">
+                <div className="navigation" style={{display: 'flex', fontFamily: 'Shrikhand', color: 'white'}}>
+                    <h3 style={{fontSize: '25pt', fontFamily: 'Shrikhand'}}>Home</h3>
+                        <Link to="/custom" style={{textDecoration: 'none', color: 'white'}}>
+                        <h3 style={{fontSize: '25pt', fontFamily: 'Shrikhand'}}>Custom</h3>
+                        </Link>
+                        <h1 style={{fontSize: '40pt', fontFamily: 'Shrikhand'}}>High Temp</h1>
+                        <Link to="/shop" style={{textDecoration: 'none', color: 'white'}}>
+                        <h3 style={{fontSize: '25pt', fontFamily: 'Shrikhand'}}>Shop</h3>
+                        </Link>
+                        <Link to="/wishlist" style={{textDecoration: 'none', color: 'white'}}>
+                        <h3 style={{fontSize: '25pt', fontFamily: 'Shrikhand'}}>Wishlist</h3>
+                        </Link>
+                </div>
+                <div className="category">
+                    <p>Pastries</p>
+                    <p>Candy</p>
+                    <p>Cakes & Pies</p>
+                    <p>Kits</p>
+                </div>
             </div>
         )
     }
